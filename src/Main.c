09c653993c60e8ed613e5985509256d0f448f802
codeleaded <gnu.c.reaper@gmail.com>
2026-04-04 16:17:43 +0200

    init commit

---
+++ src/Main.c
@@ -0,0 +1,12 @@
+#if defined __linux__
+#include "/home/codeleaded/System/Static/Library/Cmd.h"
+#elif defined _WINE
+#include "/home/codeleaded/System/Static/Library/Cmd.h"
+#elif defined _WIN32
+#include "F:/home/codeleaded/System/Static/Library/Cmd.h"
+#endif
+
+int main(int argc,const char *argv[]){
+    
+    return 0;
+}
